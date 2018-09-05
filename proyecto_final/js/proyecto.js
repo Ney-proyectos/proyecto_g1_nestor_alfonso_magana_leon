ebdca4db2f426385729dd2d80a300f6278246bd4
Ney-proyectos <maganaleon7@gmail.com>
2018-09-05 11:54:45 -0600

    proyecto final 05-09-2018
    actualizacion del menu tipo responsive

---
+++ proyecto_final/js/proyecto.js
@@ -0,0 +1,16 @@
+// funcion para resposive para que muestre el menu 
+$(window).resize(function(){
+if ($(window).width() <= 767) {
+    $("nav").hide();
+}
+else{
+    $("nav").show();
+}
+
+});
+
+$("#btn_menu").click(function(){
+    $("nav").slideToggle();
+        
+});
+
